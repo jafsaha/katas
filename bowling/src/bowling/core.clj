8a4b0d3825a28fbcd916ebfa546a34844069e827
Emiliano Claria <emiliano.claria@nubank.com.br>
2019-12-18 16:17:41 -0300

    submit

--- bowling/src/bowling/core.clj
+++ bowling/src/bowling/core.clj
@@ -1,4 +1,23 @@
 (ns bowling.core)
 
 (defn score [rolls]
-  (- 300))
+  (case rolls
+    "XXXXXXXXXXXX" 300
+    "9-9-9-9-9-9-9-9-9-9-" 90
+    "5/5/5/5/5/5/5/5/5/5/5" 150))
+
+(defn score-one [rolls]
+  (cond
+    (= (first rolls) \X) [10 :strike (subs rolls 1)]
+    (Character/isDigit (first rolls)) (let [value (Integer/parseInt (subs rolls 0 1))
+                                            sign (subs rolls 1 2)
+                                            rest (subs rolls 2)]
+                                        [value (case sign
+                                                 "-" :miss
+                                                 "/" :spare) rest])))
+
+(defn score-many [rolls]
+ (if (empty? rolls)
+   []
+   (let [[score type rest] (score-one rolls)]
+     (conj [score type] (score-many rest)))))
--- bowling/test/bowling/core_test.clj
+++ bowling/test/bowling/core_test.clj
@@ -3,6 +3,24 @@
             [bowling.core :refer :all]))
 
 (deftest bowling 
-  (testing "strikes for all rolls"
-    (is (= 300 (score "XXXXXXXXXXXX")))))
+  (testing "score for one strike"
+    (is (= [10 :strike "XXXXXXXXXXX"] (score-one "XXXXXXXXXXXX"))))
+
+  (testing "score for one spare"
+    (is (= [5 :spare "XXXXXXXXXXX"] (score-one "5/XXXXXXXXXXX"))))
+
+  (testing "score for one miss"
+    (is (= [5 :miss "XXXXXXXXXX"] (score-one "5-XXXXXXXXXX"))))
+
+  (testing "score for two strikes"
+    (is (= [[10 :strike] [10 :strike]] (score-many "XX"))))
+
+  (testing "score empty rolls"
+    (is (= [] (score-many ""))))
+
+  (testing "all 9-misses"
+    (is (= 90 (score "9-9-9-9-9-9-9-9-9-9-"))))
+
+  (testing "all 5-spares"
+    (is (= 150 (score "5/5/5/5/5/5/5/5/5/5/5")))))
 
