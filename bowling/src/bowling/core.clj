b9ac638cf14f4ebd3e437d49483170930bb2d47b
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2019-12-18 16:21:21 -0300

    seba y eze

--- bowling/src/bowling/core.clj
+++ bowling/src/bowling/core.clj
@@ -1,4 +1,14 @@
-(ns bowling.core)
+(ns bowling.core
+  (:require [clojure.string :as str]))
+
+(defn parse-roll
+  [roll]
+  (case roll
+    "X" 10
+    "-" 0
+    (Integer/parseInt roll)))
+
 
 (defn score [rolls]
-  (- 300))
+  (let [tiradas (str/split "9-9-9-9-9-9-9-9-9-9-" #"")]
+    (reduce + (map parse-roll tiradas))))
--- bowling/test/bowling/core_test.clj
+++ bowling/test/bowling/core_test.clj
@@ -2,7 +2,23 @@
   (:require [clojure.test :refer :all]
             [bowling.core :refer :all]))
 
-(deftest bowling 
+(deftest bowling
   (testing "strikes for all rolls"
-    (is (= 300 (score "XXXXXXXXXXXX")))))
+    (is (= 300 (score "XXXXXXXXXXXX"))))
 
+  (testing "20 rolls: 10 pairs of 9 and miss"
+    (is (= 90 (score "9-9-9-9-9-9-9-9-9-9-"))))
+
+  (testing "21 rolls: 10 pairs of 5 and spare, with a final 5"
+    (is (= 150 (score "5/5/5/5/5/5/5/5/5/5/5")))))
+
+
+(deftest parse-roll-test
+  (testing "X => 10"
+    (is (= 10 (parse-roll "X"))))
+
+  (testing "- => 0"
+    (is (= 0 (parse-roll "-"))))
+
+  (testing "1 => 1:int"
+    (is (= 1 (parse-roll "1")))))
