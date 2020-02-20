867ace567df64bf87b7edc658a5ef226e209d250
Sebastián Wandelow <sebastian.wandelow@nubank.com.br>
2020-02-20 16:54:48 -0300

    update project name

--- fizzbuzz/project.clj
+++ fizzbuzz/project.clj
@@ -1,4 +1,4 @@
-(defproject bowling "0.1.0-SNAPSHOT"
+(defproject fizzbuzz "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
